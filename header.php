83e6c1c746b0c026b22793fdb272bb1e8145e259
Leonardo Lanzinger <leonardo.lanzinger@gmail.com>
2013-09-18 10:14:07 +0200

    fixed template url

--- header.php
+++ header.php
@@ -9,7 +9,7 @@
     <title>Bootstrap 101 Template</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- Bootstrap -->
-    <link href="/css/bootstrap.css" rel="stylesheet" media="screen">
+    <link href="<?php bloginfo( 'template_url' ); ?>/css/bootstrap.css" rel="stylesheet" media="screen">
 
     <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!--[if lt IE 9]>
