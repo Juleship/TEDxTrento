c934bb3b75563373c7d678bae32311a6c95dc5b6
Jules <michelon.giulio@gmail.com>
2013-09-18 10:24:44 +0200

    tedx logo and html navbar

--- header.php
+++ header.php
@@ -10,6 +10,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- Bootstrap -->
     <link href="<?php bloginfo( 'template_url' ); ?>/css/bootstrap.css" rel="stylesheet" media="screen">
+    <link href="<?php bloginfo( 'template_url' ); ?>/style.css" rel="stylesheet" media="screen">
 
     <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!--[if lt IE 9]>
@@ -23,6 +24,27 @@
   </head>
 
   <body>
-
-
+  	<nav>
+	  <img src="<?php bloginfo( 'template_url' ); ?>/img/logo.png">
+	  <ul>
+	  	<li>
+	  		About
+	  	</li>
+	  	<li>
+	  		Speakers
+	  	</li>
+	  	<li>
+	  		Programma
+	  	</li>
+	  	<li>
+	  		Team
+	  	</li>
+	  	<li>
+	  		Partners
+	  	</li>
+	  	<li>
+	  		Contatti
+	  	</li>
+	  </ul>
+  	</nav>
 <!-- navbar -->
