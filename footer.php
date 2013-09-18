1c5098eef6033a562740e63429d3f52b5e1c793e
Jules <michelon.giulio@gmail.com>
2013-09-18 10:10:24 +0200

    we have bootstrap and good header and footer too

--- footer.php
+++ footer.php
@@ -1,4 +1,4 @@
-<?php
+	<?php
 	wp_footer();
 	?>
 	
@@ -6,5 +6,9 @@
 	foobar, credits, contacts
 	scripts
 -->
-</body>
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="//code.jquery.com/jquery.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+  </body>
 </html>
--- header.php
+++ header.php
@@ -2,12 +2,27 @@
 	html includes etc etc (stylesheets etc etc)
 	navbar
 -->
+
+<!DOCTYPE html>
 <html>
-<head>
-<?php
+  <head>
+    <title>Bootstrap 101 Template</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
+
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="../../assets/js/html5shiv.js"></script>
+      <script src="../../assets/js/respond.min.js"></script>
+    <![endif]-->
+    
+    <?php
 	wp_head();
-?>
-</head>
-<body>
+	?>
+  </head>
+
+  <body>
+
 
 <!-- navbar -->
