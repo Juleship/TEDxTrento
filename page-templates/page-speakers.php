0b4b3830a8707da4f30b1c99113a534d9e4f23d2
Leonardo Lanzinger <leonardo.lanzinger@gmail.com>
2013-09-19 19:25:41 +0200

    speaker page with some styling

--- page-templates/page-speakers.php
+++ page-templates/page-speakers.php
@@ -3,8 +3,10 @@
 	*/
 	get_header(); 
 ?>
-	<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-	<?php the_content(); ?>
-	<?php endwhile; endif; ?>
-	
+	<div class="col-sm-12">
+		<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+		<?php the_content(); ?>
+		<?php endwhile; endif; ?>
+		<div style="clear: both;"></div>
+	</div>
 <?php get_footer(); ?>
