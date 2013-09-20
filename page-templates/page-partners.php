67c9330fbada90c30ee2476c6c8aaffb0bfc2123
Leonardo Lanzinger <leonardo.lanzinger@gmail.com>
2013-09-20 10:57:13 +0200

    partners page with some styling

--- page-templates/page-partners.php
+++ page-templates/page-partners.php
@@ -2,7 +2,25 @@
 	/* Template Name: Tedx Partners Page
 	*/
 	get_header(); 
+	$posttitle = 'partners-intro';
+	$postid = $wpdb->get_var( "SELECT ID FROM $wpdb->posts WHERE post_title = '" . $posttitle . "'" );
+	$postid;
 ?>
-	
+	<div class="col-sm-12 padded-row">
+		<div class="col-sm-8">
+			<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+			<?php the_content(); ?>
+			<?php endwhile; endif; ?>
+		</div>
+		<div class="col-sm-4 partners-intro">
+			<?php
+				$getpost= get_post($postid);
+				$postcontent= $getpost->post_content;
+				echo $postcontent;
+			?>
+		</div>
+		<div style="clear:both;">
+		</div>
+	</div>
 	
 <?php get_footer(); ?>
--- page-templates/page-speakers.php
+++ page-templates/page-speakers.php
@@ -1,12 +1,25 @@
 <?php 
 	/* Template Name: Tedx Speakers Page
 	*/
-	get_header(); 
+	get_header();
+	$posttitle = 'speakers-intro';
+	$postid = $wpdb->get_var( "SELECT ID FROM $wpdb->posts WHERE post_title = '" . $posttitle . "'" );
+	$postid;
 ?>
 	<div class="col-sm-12">
 		<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 		<?php the_content(); ?>
 		<?php endwhile; endif; ?>
 		<div style="clear: both;"></div>
+		<div class="row padded-row">
+			<div class="col-sm-8 col-sm-offset-2 speakers-intro">
+				<hr />
+				<?php
+				$getpost= get_post($postid);
+				$postcontent= $getpost->post_content;
+				echo $postcontent;
+				?>
+			</div>
+		</div>
 	</div>
 <?php get_footer(); ?>
