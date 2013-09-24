c7175c694c33c3327c24171de07ab2903489033d
Jules <michelon.giulio@gmail.com>
2013-09-24 10:52:54 +0200

    first page beginnning

--- index.php
+++ index.php
@@ -1,8 +1,30 @@
 <?php get_header(); ?>
 
 
-<div class="text-center">
-	<img src="<?php bloginfo( 'template_url' ); ?>/img/splash.png">
-</div>
-
+	<div class="row home-background">
+		<div class="col-sm-10 col-md-8 col-sm-offset-1 col-md-offset-2 partners-intro text-center">
+			<h2 class="main-title">Sai cos'é TEDx Trento?</h2>
+			<p>
+				Il 23/11 si terrà a Trento uno degli eventi più rivoluzionari della storia dell'umanità. Non sarà solo una conferenza ma una vera e propria fucina di idee. Come puoi mancare?
+			</p>
+			<h2><strong>23 Novembre 2013</strong><br>annotalo sul taccuino!</h2>
+		</div>
+	</div>
+	
+	<div class="row">			
+		<div class="col-sm-10 col-md-8 col-sm-offset-1 col-md-offset-2 text-center buy-tickets">
+			<button class="btn btn-default">COMPRA I BIGLIETTI</button>
+		</div>
+	</div>
+	
+	<div class="row bottom-home-background">
+		<div class="col-sm-10 col-md-8 col-sm-offset-1 col-md-offset-2 partners-intro text-center">
+			<h2>Mancano ancora</h2>
+			<p>
+				Un tot di ore minuti giorni roba
+			</p>
+			<h2>a TEDxTrento</h2>
+		</div>
+	</div>
+		
 <?php get_footer(); ?>
