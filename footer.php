0650e79bf74c3f0d22f5ede2a2277607405c8278
Leonardo Lanzinger <leonardo.lanzinger@gmail.com>
2013-09-24 16:53:38 +0200

    almost done

--- footer.php
+++ footer.php
@@ -6,7 +6,7 @@
 			<img src="<?php bloginfo( 'template_url' );?>/img/small-logo.png" class="footer-logo"/>
 			<div class="hidden-sm hidden-xs">
 				<small>
-					<p>What is TEDx?</p> 
+					<p>What is <a href="http://www.ted.com/tedx">TEDx</a>?</p> 
 					<p>In the spirit of ideas worth spreading, TED has created a program called TEDx. TEDx is a program of local, self-organized events that bring people together to share a TED-like experience. Our event is called TEDx[name], where x = independently organized TED event. At our TEDx[name] event, TEDTalks video and live speakers will combine to spark deep discussion and connection in a small group. The TED Conference provides general guidance for the TEDx program, but individual TEDx events, including ours, are self-organized. </p>
 				</small>
 			</div>
--- index.php
+++ index.php
@@ -7,21 +7,25 @@
 			<p>
 				Il 23/11 si terrà a Trento uno degli eventi più rivoluzionari della storia dell'umanità. Non sarà solo una conferenza ma una vera e propria fucina di idee. Come puoi mancare?
 			</p>
+			<hr />
 			<h2><strong>23 Novembre 2013</strong><br>segna la data in agenda!</h2>
+			
+			<a href="http://www.boxol.it/PrimiAllaPrima/IT/PrimiAllaPrima-dettaglio-evento.aspx?E=78657"><button class="btn btn-default babbies-button">Acquista i biglietti<img src="<?php bloginfo( 'template_url' ); ?>/img/primi_alla_prima.png" /></button></a>
 		</div>
 	</div>
 	
 	<div class="row home-row">			
-		<div id="tickets" class="col-sm-10 col-md-8 col-sm-offset-1 col-md-offset-2 text-center index-big-padding">
-			<p>
-				Prenota i biglietti per l'evento:
-			</p>
-			<div class="ticket-form">
-				nome:<input></input>
-				cognome:<input></input>
-				e-mail:<input></input>
-				<br />
-				<button class="btn btn-default babbies-button">PRENOTA</button>
+		<div id="tickets" class="col-sm-10 col-sm-offset-1 text-center index-big-padding">
+			
+			<div class="col-sm-5">
+				<p class="text-left">
+					Enthusiastically monetize integrated models via high standards in process improvements. Proactively empower one-to-one applications and backend customer service and a <a href="">hyper-link</a> here.</p>
+				<p class="text-left">
+					Energistically reinvent holistic products without customer directed potentialities. And something else here to say hello goodbye and something else again and again.
+				</p>
+			</div>
+			<div class="col-sm-7">
+				<iframe width="100%" height="300px" src="//www.youtube.com/embed/p7O9O6Tzp6A" frameborder="0" allowfullscreen></iframe>
 			</div>
 		</div>
 	</div>
